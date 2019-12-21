3c91a74ed7c04d055c12e4be1ffcce0c5aeaf1d5
JosephWyman328 <Jwyman@student.framingham.edu>
2019-12-21 10:58:33 -0500

    try new database for posgres

--- meditation_app_backend/settings.py
+++ meditation_app_backend/settings.py
@@ -86,12 +86,10 @@ WSGI_APPLICATION = 'meditation_app_backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+    }}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
