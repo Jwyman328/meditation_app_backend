a682d4154982e89b0cef7ffa02f8110fd560ee4f
JosephWyman328 <Jwyman@student.framingham.edu>
2020-01-05 17:03:45 -0500

    get user friend data

--- data/views.py
+++ data/views.py
@@ -44,7 +44,7 @@ class getMyFriends(views.APIView):
         my_query = userAdditions.objects.filter(user=request.user)
 
         #serialized_data = userAdditionsSerializer(my_query, many=True).data
-        serialized_data = UserSerializer(my_query.friends, many=True).data
+        serialized_data = UserSerializer(my_query[0].friends, many=True).data
 
         return Response(serialized_data, status.HTTP_200_OK)
 
