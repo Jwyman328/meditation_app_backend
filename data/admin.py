7641cb171e6e231b87580879983ef15ac0380065
JosephWyman328 <Jwyman@student.framingham.edu>
2020-01-05 14:37:49 -0500

    Make userAddition Migration

--- data/admin.py
+++ data/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import AudioMeditation, MeditationCatagoryType, MeditationCourse, UserCatagories
+from .models import AudioMeditation, MeditationCatagoryType, MeditationCourse, UserCatagories, userAdditions
 
 admin.site.register(AudioMeditation)
 admin.site.register(MeditationCatagoryType)
 admin.site.register(MeditationCourse)
 admin.site.register(UserCatagories)
+admin.site.register(userAdditions)
 
 
