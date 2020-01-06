20b049195340423f0bfbb771955e6fb05ad8d77d
JosephWyman328 <Jwyman@student.framingham.edu>
2020-01-06 09:51:07 -0500

    Add Journal entry model to admin

--- data/admin.py
+++ data/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import AudioMeditation, MeditationCatagoryType, MeditationCourse, UserCatagories, userAdditions
+from .models import JournalEntry, AudioMeditation, MeditationCatagoryType, MeditationCourse, UserCatagories, userAdditions
 
 admin.site.register(AudioMeditation)
 admin.site.register(MeditationCatagoryType)
 admin.site.register(MeditationCourse)
 admin.site.register(UserCatagories)
 admin.site.register(userAdditions)
+admin.site.register(JournalEntry)
+
 
 
