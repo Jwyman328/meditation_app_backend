6ebe65565ed5668ba12182115a51abee2904dcfc
Joseph Wyman <jwyman@Josephs-MBP.home>
2020-01-14 10:02:32 -0500

    Add additional data url end point

--- data/urls.py
+++ data/urls.py
@@ -40,7 +40,4 @@ urlpatterns = [
     path('ReturnAudio', views.ReturnAudio.as_view(), name='return_audio'),
     path('sign_up_additional_data', views.sign_up_additional_data.as_view(), name='sign_up_additional_data'),
 
-
-
-
 ]
