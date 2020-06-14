afeb315725d31928045f5a25ebb074eef55556c7
Joseph Wyman <jwymandev@gmail.com>
2020-06-14 15:46:00 -0400

    Add mediation listen results to admin

--- data/admin.py
+++ data/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import audio_field_test, FitnessGoals, MyFeelings, DirectMessage, FriendRequest, JournalEntry, AudioMeditation, MeditationCatagoryType, MeditationCourse, UserCatagories, userAdditions
+from .models import audio_field_test, FitnessGoals, MyFeelings, DirectMessage, FriendRequest, JournalEntry, AudioMeditation, MeditationCatagoryType, MeditationCourse, UserCatagories, userAdditions, MeditationListenResults
 
 admin.site.register(AudioMeditation)
 admin.site.register(MeditationCatagoryType)
@@ -14,6 +14,7 @@ admin.site.register(DirectMessage)
 admin.site.register(MyFeelings)
 admin.site.register(FitnessGoals)
 admin.site.register(audio_field_test)
+admin.site.register(MeditationListenResults)
 
 
 
